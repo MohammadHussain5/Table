1f21d6e0b03447a3d6a00ceac9be3e41b92fb8a0
Mohammad Hussain <165736304+MohammadHussain5@users.noreply.github.com>
2024-05-09 03:42:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Creating a table function
+function table(num1 = 0) {
+    console.log(num1 + " x 1 = " + num1 * 1);
+    console.log(num1 + " x 2 = " + num1 * 2);
+    console.log(num1 + " x 3 = " + num1 * 3);
+    console.log(num1 + " x 4 = " + num1 * 4);
+    console.log(num1 + " x 5 = " + num1 * 5);
+    console.log(num1 + " x 6 = " + num1 * 6);
+    console.log(num1 + " x 7 = " + num1 * 7);
+    console.log(num1 + " x 8 = " + num1 * 8);
+    console.log(num1 + " x 9 = " + num1 * 9);
+    console.log(num1 + " x 10 = " + num1 * 10);
+}
+table(5);
+table(1000);
+table();
+export {};
